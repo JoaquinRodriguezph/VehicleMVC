ad1ff43e0b851cb740fd19515d4556fd76e4f666
gokstan <stanley_yale_zeng@dlsu.edu.ph>
2023-07-20 14:57:40 +0800

    hi

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,10 @@
 public abstract class Vehicle {
+    protected Vehicle(int capacity, int passengers) {
+        this.CAPACITY = capacity;
+        this.passengers = passengers;
+    }
+    private final int CAPACITY;
+    private String startPoint;
+    private String endPoint;
+    private int passengers;
 }
