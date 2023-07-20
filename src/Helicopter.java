7d17e829f05a937a7adf7c6b83f5b131ebb8fb51
gokstan <stanley_yale_zeng@dlsu.edu.ph>
2023-07-20 15:13:18 +0800

    big push

--- src/Helicopter.java
+++ src/Helicopter.java
@@ -1,2 +1,5 @@
 public class Helicopter extends Vehicle {
+    public Helicopter(int capacity, int passengers, String startPoint, String endPoint) {
+        super(capacity, passengers, startPoint, endPoint);
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,8 +1,27 @@
 public abstract class Vehicle {
-    protected Vehicle(int capacity, int passengers) {
+    protected Vehicle(int capacity, int passengers, String startPoint, String endPoint) {
         this.CAPACITY = capacity;
         this.passengers = passengers;
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
     }
+
+    public int getCapacity() {
+        return CAPACITY;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
     private final int CAPACITY;
     private String startPoint;
     private String endPoint;
