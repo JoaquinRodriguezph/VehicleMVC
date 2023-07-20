58fa034b630b2c48fe9b6dac20866099022858db
quin <joaquin_rodriguez@dlsu.edu.ph>
2023-07-20 15:33:21 +0800

    ConsoleDriver things

--- src/ConsoleDriver.java
+++ src/ConsoleDriver.java
@@ -16,14 +16,17 @@ public class ConsoleDriver {
             System.out.println("Invalid type. Please enter an integer.");
         }
 
-        for (int n: numofvehicles){
+        for (int i = 0; i < numofvehicles; i++){
             System.out.println("Please choose type of vehicle: ");
+            System.out.println("1 - Bus");
+            System.out.println("2 - Jeepney");
+            System.out.println("3 - Helicopter");
             int choice = 0;
             try{
                 choice = sc.nextInt();
             }
             catch(InputMismatchException e){
-
+                System.out.println("Please input only from 1 to 4");
             }
         }
 
