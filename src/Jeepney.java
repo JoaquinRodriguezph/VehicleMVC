c03b0deed7522c42c59a3c44a01adbf444ea2f89
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2023-07-23 12:13:11 +0800

    Update Jeepney.java

--- src/Jeepney.java
+++ src/Jeepney.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
 
+/**
+ * The Jeepney class represents a public vehicle that extends the Vehicle class and implements the Public interface.
+ * It provides methods to compute the fare for passengers traveling on the jeepney.
+ */
 public class Jeepney extends Vehicle implements Public{
+
+    /**
+    * Constructs a Jeepney object with the given travelled starting point and ending point.
+    *
+    * @param startPoint The starting point of the Jeepney's travelled distance.
+    * @param endPoint   The end point of the Jeepney's travelled distance.
+    */
     public Jeepney(String startPoint, String endPoint) {
         super(20, startPoint, endPoint);
     }
 
+    /**
+     * Computes the fare for passengers traveling on the jeepney for a given distance.
+     * The fare is P7 for the first 4 kilometers, and an additional P1 for every extra kilometer.
+     *
+     * @param km The distance traveled by the jeepney in kilometers.
+     * @return The total fare charged for all passengers on the jeepney.
+     */
     @Override
     public int computeFair(int km) {
         int sum = 0;
