4b2987e5b096b7df9acb9c5d2c7fe9f3aceca900
Stanley <stanley_yale_zeng@dlsu.edu.ph>
2023-07-23 14:26:08 +0800

    push pushn

--- src/Private.java
+++ src/Private.java
@@ -1,14 +1,14 @@
 
 /**
  * The Private interface represents vehicles that are not available for public transportation.
- * Vehicles that implement this interface should provide a method to update the distance traveled.
+ * Vehicles that implement this interface should provide a method to update the necessary features and implementations.
  */
 public interface Private {
 
     /**
     * Updates the distance traveled by the private vehicle.
     *
-    * @param km The distance traveled by the public vehicle in kilometers.
+    * @param km The distance traveled by the private vehicle in kilometers.
     */
     public abstract void travel(int km);
 }
