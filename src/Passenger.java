d062affa3e28c828a20afb7f705ef3792a7c94af
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2023-07-23 12:23:06 +0800

    Update Passenger.java

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,16 +1,41 @@
+/**
+* The Passenger class represents a passenger who can travel a certain distance using different vehicles.
+* The passenger has a total distance to travel and the distance traveled.
+*/
 public class Passenger {
+    
+    /**
+    * Constructs a Passenger object with the given total distance to travel.
+    *
+    * @param km The total distance to travel in kilometers.
+    */
     public Passenger (int km) {
         distance = km;
     }
-
+    
+    /**
+    * Get the distance the passenger needs to travel.
+    *
+    * @return The distance in kilometers.
+    */
     public int getDistance() {
         return distance;
     }
-
+    
+    /**
+    * Get the distance traveled by the passenger.
+    *
+    * @return The distance traveled in kilometers.
+    */
     public int getDistanceTraveled() {
         return distanceTraveled;
     }
 
+    /**
+    * Calculates the remaining distance and the distance traveled.
+    *
+    * @param km The distance traveled in kilometers.
+    */
     public void travel(int km) {
         distance -= km;
         distanceTraveled += km;
