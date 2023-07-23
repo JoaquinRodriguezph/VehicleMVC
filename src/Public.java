1982fc8505560d56c6a7d2dd2cf257e726ec5fca
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2023-07-23 12:28:58 +0800

    Update Public.java

--- src/Public.java
+++ src/Public.java
@@ -1,3 +1,12 @@
+/**
+ * The Public interface represents a public vehicle and provides a method to compute the fare
+ * based on the distance traveled.
+ */
 public interface Public {
+    /**
+    * Computes the fare for a public vehicle based on the distance traveled in kilometers.
+    *
+    * @param km The distance traveled by the public vehicle in kilometers.
+    */
     public abstract int computeFair(int km);
 }
