aa886f488ae24b36503ba198ba239fb4427e2e20
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2023-07-23 12:27:08 +0800

    Update Private.java

--- src/Private.java
+++ src/Private.java
@@ -1,3 +1,13 @@
+
+/**
+ * The Public interface represents a public vehicle and provides a method to compute the fare
+ * based on the distance traveled.
+ */
 public interface Private {
+    /**
+    * Computes the fare for a public vehicle based on the distance traveled in kilometers.
+    *
+    * @param km The distance traveled by the public vehicle in kilometers.
+    */
     public abstract void travel(int km);
 }
